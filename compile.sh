a8b1dc844cbebb085f4fdb7ea7af7db39fe8d7ff
Eli Wenig <eli@csh.rit.edu>
2015-04-28 12:03:13 -0700

    fixing include folder in compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-64tass -C -T -a -I ~/dev/c64/inc -i ${1}/${1}.asm -o ${1}/${1}.tmp
+64tass -C -T -a -I inc/ -i ${1}/${1}.asm -o ${1}/${1}.tmp
 echo "====================================="
 pucrunch ${1}/${1}.tmp ${1}/${1}.prg -x 4096 -ffast -m5 
 rm ${1}/${1}.tmp
