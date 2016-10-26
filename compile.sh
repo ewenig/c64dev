5fb35fb245d194510668b07822f4451bfca0b639
Eli Wenig <eli@csh.rit.edu>
2016-10-25 17:23:57 -0700

    placing all prgs in one PROGRAMS/ folder

--- compile.sh
+++ compile.sh
@@ -3,5 +3,6 @@
 PATH=$PATH:bin
 64tass -C -a -I inc/ -i ${1}/`basename ${1}`.asm -o ${1}/`basename ${1}`.tmp
 echo "====================================="
-pucrunch ${1}/`basename ${1}`.tmp ${1}/`basename ${1}`.prg -x 4096 -ffast -m5 
+basename=`basename ${1}`
+pucrunch ${1}/`basename ${1}`.tmp PROGRAMS/${basename^^}.PRG -x 4096 -ffast -m5
 rm ${1}/`basename ${1}`.tmp
